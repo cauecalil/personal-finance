ed78af7d405078876205d7864bf9d58934116aad
cauecalil <caue.26@hotmail.com>
2026-04-16 20:32:06 -0300

    feat(test): add H2 integration test configuration

---
+++ backend/src/test/java/com/cauecalil/personalfinance/support/H2UseCaseIntegrationTest.java
@@ -0,0 +1,11 @@
+package com.cauecalil.personalfinance.support;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.transaction.annotation.Transactional;
+
+@SpringBootTest
+@ActiveProfiles("test")
+@Transactional
+public abstract class H2UseCaseIntegrationTest {
+}
